2283bd9b31ddb9401d33c43a5fa8dc3337e58e01
Gh4m21 <gervoos@gmail.com>
2019-09-18 12:40:31 -0400

    homeNotConnected.html design done at 95 %

---
+++ organizExpenses.js
@@ -0,0 +1,49 @@
+// Javascript functions for login and sign up transitions
+var linkLogin = document.querySelector('.form .register-form .login');
+var linkRegister = document.querySelector('.form .login-form .register');
+
+
+//with the form 
+
+linkLogin.addEventListener('click', function () {
+    $(".form .login-form").css("display", "block");
+    $(".form .register-form").css("display", "none");
+    menuLinkRegister.classList.remove("active");
+    menuLinkLogin.classList.add("active");
+
+});
+
+
+linkRegister.addEventListener('click', function () {
+    $(".form .login-form").css("display", "none");
+    $(".form .register-form").css("display", "block");
+    menuLinkRegister.classList.add("active");
+    menuLinkLogin.classList.remove("active");
+
+});
+
+//with the menu link
+
+var menuLinkLogin = document.querySelector('nav .nav-item .login');
+var menuLinkRegister = document.querySelector('nav .nav-item .register');
+
+menuLinkRegister.addEventListener('click', function () {
+    $(".form .login-form").css("display", "none");
+    $(".form .register-form").css("display", "block");
+    menuLinkRegister.classList.add("active");
+    menuLinkLogin.classList.remove("active");
+
+});
+
+
+menuLinkLogin.addEventListener('click', function () {
+    $(".form .login-form").css("display", "block");
+    $(".form .register-form").css("display", "none");
+    menuLinkRegister.classList.remove("active");
+    menuLinkLogin.classList.add("active");
+
+});
+
+
+
+//Statistics bar: pie javascript
